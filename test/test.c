65050da4f0d05f786c8ecebf652b50fd20fc64b8
marcos lott <lott.marcos@gmail.com>
2023-10-11 18:01:06 -0300

    Alterando a função Test

--- test/test.c
+++ test/test.c
@@ -1,16 +1,21 @@
 #include <criterion/criterion.h> // Inclui a biblioteca Criterion
 
 // Função para verificar se um valor está dentro do intervalo razoável
-int reasonable_values(float value){
-    if(value > 8.0 && value < 35.0){
+int reasonable_values(float value)
+{
+    if (value > 8.0 && value < 35.0)
+    {
         return 1; // Retorna 1 se o valor estiver dentro do intervalo
-    } else{
+    }
+    else
+    {
         return 0; // Retorna 0 se o valor estiver fora do intervalo
-    }  
+    }
 }
 
 // Teste para verificar a função reasonable_values
-Test(suite_name, test_name){
+Test(suite_name, test_name)
+{
     // Verifica se a função retorna 1 quando o valor 25.0 é atribuído
-    cr_assert(reasonable_values(25.0) == 1);
+    cr_assert(reasonable_values(45.0) == 1);
 }
