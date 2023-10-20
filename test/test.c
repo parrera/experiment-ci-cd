5501d9b2cc49ec7ea8ee3c189cf396e79ff6f977
Carol-Galarani <mcarolinagf@hotmail.com>
2023-10-20 14:39:34 -0300

    Consertando a função Test

--- test/test.c
+++ test/test.c
@@ -12,5 +12,5 @@ int reasonable_values(float value){
 // Teste para verificar a função reasonable_values
 Test(suite_name, test_name){
     // Verifica se a função retorna 1 quando o valor 25.0 é atribuído
-    cr_assert(reasonable_values(45.0) == 1);
+    cr_assert(reasonable_values(26.0) == 1);
 }
